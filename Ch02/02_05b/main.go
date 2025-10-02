6ad7b02985956fde3ec0d2c14a7f051d05ec76c2
divantsov <daniil_ivantsov@epam.com>
2025-10-02 04:29:40 +0000

    ch2

--- Ch02/02_05b/main.go
+++ Ch02/02_05b/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func main() {
@@ -10,4 +11,15 @@ func main() {
 	sum := f1 + f2 + f3
 	fmt.Println("Float sum:", sum)
 
+	sum = math.Round(sum * 100) / 100
+
+	fmt.Printf("The sum is now %v\n\n", sum)
+
+	fmt.Println("The value of PI is", math.Pi)
+
+	circleRad := 15.5
+
+	curcumference := circleRad *2 * math.Pi
+
+	fmt.Printf("Curcumference: %.2f\n", curcumference)
 }
