04079174e419f134faa9688904fb724d26a852a4
divantsov <daniil_ivantsov@epam.com>
2025-10-02 10:03:00 +0000

    ch4

--- Ch03/03_04b/main.go
+++ Ch03/03_04b/main.go
@@ -2,10 +2,24 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
 func main() {
 	// This is an array
-	var colors = [3]string{"Red", "Green", "Blue"}
+	//var colors = [3]string{"Red", "Green", "Blue"}
+	var colors = make([]string, 0, 3)
+	colors = append(colors, "Red", "Green", "Blue")
+	colors = append(colors, "Purple", "Fuschia")
+	
+	colors = remove(colors, 1)
+
 	fmt.Println(colors)
+
+	sort.Strings(colors)
+	fmt.Println(colors)
+}
+
+func remove(slice []string, i int) []string {
+	return append(slice[:i], slice[i+1:]...)
 }
--- Ch04/04_03b/main.go
+++ Ch04/04_03b/main.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func main() {
 
-	// colors := []string{"Red", "Green", "Blue"}
+	//colors := []string{"Red", "Green", "Blue"}
 	// for i := 0; i < len(colors); i++ {
 	// 	println(colors[i])
 	// }
@@ -13,17 +13,17 @@ func main() {
 	// 	println(colors[i])
 	// }
 
-	// for _, color := range colors {
+	// for _, color :=range colors {
 	// 	println(color)
 	// }
 
-	// states := make(map[string]string)
-	// states["WA"] = "Washington"
-	// states["OR"] = "Oregon"
-	// states["CA"] = "California"
-	// for state, _ := range states {
-	// 	println(states[state])
-	// }
+	states := make(map[string]string)
+	states["WA"] = "Washington"
+	states["OR"] = "Oregon"
+	states["CA"] = "California"
+	for state, _ := range states {
+		println(states[state])
+	}
 
 	value := 0
 	sum := 0
@@ -43,5 +43,4 @@ func main() {
 	}
 	theEnd : println("end of program")
 	fmt.Printf("Sum: %v\n", sum)
-
 }
